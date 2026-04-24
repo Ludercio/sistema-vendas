04919c75d2ea3e697ae224ab088102faaa0cb5b3
Hidilson45 <hidilson45@hotmail.com>
2026-04-24 21:13:35 +0200

    feat: remove product and stock visualization on admin view

--- js/controllers/AppControllers.js
+++ js/controllers/AppControllers.js
@@ -96,10 +96,20 @@ export class AppControllers {
         this.atualizarInterface();
     }
 
+    removerProduto(id) {
+    const index = this.produtos.findIndex(p => p.id === id);
+    if (index !== -1) {
+        const nome = this.produtos[index].nome;
+        this.produtos.splice(index, 1);
+        alert(`Produto "${nome}" removido com sucesso!`);
+        this.atualizarInterface();
+    }
+}
+
     // Atualiza todos os componentes visuais de uma vez
     atualizarInterface() {
         this.view.renderizarCatalogo(this.produtos, (id) => this.comprar(id));
         this.view.renderizarResumoCarrinho(this.carrinho);
-        this.view.renderizarTabelaAdmin(this.produtos);
+        this.view.renderizarTabelaAdmin(this.produtos, (id) => this.removerProduto(id));
     }
 }
